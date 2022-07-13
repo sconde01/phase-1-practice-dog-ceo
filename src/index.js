58d5c5f62102fc5c96feaf7a51be793d66b41f2a
Stephanie Conde <stephaniecondecareer@gmail.com>
2022-07-12 20:10:08 -0700

    commit tree 862a6f8c067454c723833b9f9cd7fdb1955633c6

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded" , function(){
+let dogImageContainer = document.getElementById('dog-image-container')
+
+fetch("https://dog.ceo/api/breeds/image/random/4")
+.then(response => response.json())
+.then(function(jsonObject){
+    let arrOfDogURLs = jsonObject.message
+    arrOfDogURLs.forEach(url => {
+      dogImageContainer.innerHTML += makeImageTagString(url)
+    })
+})
+
+})
+
+function makeImageTagString(url){
+  return `<img src="${url}"/>`
+}
